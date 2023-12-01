c4db168cd81c4759fb6b4becc8c108c94808c345
muss001998 <mustapha001hamiai@gmail.com>
2023-11-30 16:28:15 -0800

    python-import_modules

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+def add(a, b):
+    """Here is my addition function
+
+    Args:
+        a: first int
+        b: second int
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
+
+
+def sub(a, b):
+    """Here is my subtraction function
+
+    Args:
+        a: first int
+        b: second int
+
+    Returns:
+        The return value. a - b
+    """
+    return (a - b)
+
+
+def mul(a, b):
+    """Here is my multiplication function
+
+    Args:
+        a: first int
+        b: second int
+
+    Returns:
+        The return value. a * b
+    """
+    return (a * b)
+
+
+def div(a, b):
+    """Here is my division function
+
+    Args:
+        a: first int
+        b: second int
+
+    Returns:
+	 The return value. a / b
+    """
+    return int(a / b)
---
+++ 0x02-python-import_modules/variable_load_5.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+a = 98
+"""This is my simple variable
+"""

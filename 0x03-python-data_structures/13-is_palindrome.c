5ce0690a2ac3022a5ff9f771f3f2c575fb8cb1bc
muss001998 <mustapha001hamiai@gmail.com>
2023-12-04 15:02:20 -0800

    Python - Data Structures: Lists, Tuples

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,53 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+*add_nodeint by muss -this will adds a new node at the beginning of a listint_t list
+*@head: this will always put head of listint_t
+*@n: integer that will add in listint_t list
+*Return:this is the  address of the new element, or NULL if it failed
+*/
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
+/**
+*is_palindrome - this is the identify if a syngle linked list is palindrome
+*@head:this will always put  head of listint_t
+*Return:this is the return statement which returns 1 if it is palindrome else 0
+*/
+int is_palindrome(listint_t **head)
+{
+	listint_t *head2 = *head;
+	listint_t *aux = NULL, *aux2 = NULL;
+
+	if (*head == NULL || head2->next == NULL)
+		return (1);
+	while (head2 != NULL)
+	{
+		add_nodeint(&aux, head2->n);
+		head2 = head2->next;
+	}
+	aux2 = aux;
+	while (*head != NULL)
+	{
+		if ((*head)->n != aux2->n)
+		{
+			free_listint(aux);
+			return (0);
+		}
+		*head = (*head)->next;
+		aux2 = aux2->next;
+	}
+	free_listint(aux);
+	return (1);
+}
---
+++ 0x03-python-data_structures/3-main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
+
+my_list = [1, 2, 3, 4, 5]
+print_reversed_list_integer(my_list)
+
+guillaume@ubuntu:~/0x03$ ./3-main.py
+5
+4
+3
+2
+1
